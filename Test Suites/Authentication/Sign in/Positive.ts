23bb80546290bb6206389ba20df8d601625b5207
AliAsad1432 <aliasadshaheen@gmail.com>
2024-09-03 09:04:21 -0700

    New Changes

--- Test Suites/Authentication/Sign in/Positive.ts
+++ Test Suites/Authentication/Sign in/Positive.ts
@@ -12,20 +12,6 @@
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>1c4a3ace-f475-4898-b68e-b4cc97ef3dfc</testSuiteGuid>
    <testCaseLink>
-      <guid>5ad247bd-83fa-4aaa-9d54-7e65b9496cdf</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Authentication Module/Sign in/Positive Test Cases/Sign in (Remember Me Selected)</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>3060662f-8f36-4915-8ccd-3221d17087c7</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Authentication Module/Sign in/Positive Test Cases/Sign in (Uncheck Remember Me)</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
       <guid>8a6c856e-d8b8-4970-b10d-f83d539eb7bc</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
